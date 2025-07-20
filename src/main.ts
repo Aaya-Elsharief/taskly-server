fd179c79acec6ebb3eea5b1baf6dbaa90d2fed0e
Aaya-Elsharief <aaya.elsh@gmail.com>
2025-07-20 19:59:24 +0300

    swagger setup #1

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,21 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EnvironmentVariables } from 'env/env.configuration';
 import { AllExceptionsFilter } from './utils/exception-filter/all-exceptions.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter(app.get(HttpAdapterHost)));
+
+  const config = new DocumentBuilder()
+    .setTitle('Taskly API')
+    .setDescription('The API documentation for Taskly application')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(EnvironmentVariables().port);
 }
